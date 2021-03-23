b46c7336be50f101e22195da1abbd1628307069a
Bhagwan Singh <bhagwan.5602@gmail.com>
2021-03-23 18:19:07 +0530

    comments added on accounts view

--- mmnotes/accounts/views.py
+++ mmnotes/accounts/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def login_view(request):
+    #if request.method == 'POST':
+        # Instanciate Login Form
+        # Validate Loin Form
+            #check form data validation
+        #if validation failed raise error
     return render(request, "account/login.html", context=None)
 
 def register_view(request):
