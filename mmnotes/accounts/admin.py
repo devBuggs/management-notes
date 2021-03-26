c5fb2c91a1f07b8ff5cc3ff767b615dbe8e19b64
Bhagwan Singh <bhagwan.5602@gmail.com>
2021-03-26 14:25:57 +0530

    subscription models added in account
    SubscriptionPack and UserSubscription is created and migration is done

--- mmnotes/accounts/admin.py
+++ mmnotes/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import SubscriptionPack, UserSubscription
+
+
 # Register your models here.
+admin.site.register(SubscriptionPack)
+admin.site.register(UserSubscription)
--- mmnotes/accounts/models.py
+++ mmnotes/accounts/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
+class SubscriptionPack(models.Model):
+    subscription = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.subscription
+
+class UserSubscription(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    subscription_details = models.ForeignKey(SubscriptionPack, on_delete=models.CASCADE)
