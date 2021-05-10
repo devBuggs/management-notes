461ca0cad5f81242ee22c85c1febf65ae369c9ac
Bhagwan Singh <bhagwan.5602@gmail.com>
2021-05-10 17:22:51 +0530

    Administration detailed sections added

--- mmnotes/payment/admin.py
+++ mmnotes/payment/admin.py
@@ -4,6 +4,10 @@ from django.contrib import admin
 # Import models here.
 from .models import Transaction
 
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('made_by', 'order_id', 'made_on')
+    list_filter = ['made_on']
+
 
 # Register your models here.
-admin.site.register(Transaction)
+admin.site.register(Transaction, TransactionAdmin)
--- mmnotes/webapp/admin.py
+++ mmnotes/webapp/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import contact
 
+class contactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject')
+
 # Register your models here.
-admin.site.register(contact)
+admin.site.register(contact, contactAdmin)
