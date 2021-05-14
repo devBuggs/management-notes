601a636a205464ea7bf496d9cea0fb8fb83a81ab
Bhagwan Singh <bhagwan.5602@gmail.com>
2021-05-14 21:25:03 +0530

    client review is added to administration

--- mmnotes/webapp/admin.py
+++ mmnotes/webapp/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import contact
+from .models import contact, ClientReview
 
 class contactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'subject')
 
+class ClientReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message')
+
 # Register your models here.
 admin.site.register(contact, contactAdmin)
+admin.site.register(ClientReview, ClientReviewAdmin)
