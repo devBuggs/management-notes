9680abbcc76ebbed351af823e07c1895e2b32d78
Bhagwan Singh <bhagwan.5602@gmail.com>
2021-05-14 21:31:25 +0530

    alter client review model

--- mmnotes/webapp/models.py
+++ mmnotes/webapp/models.py
@@ -14,5 +14,4 @@ class contact(models.Model):
 class ClientReview(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
-    message = models.CharField(max_length=100)
-    
+    message = models.CharField(max_length=500)
