0110b1c714f9b01481e7e0289ef4183c51001aaa
Bhagwan Singh <bhagwan.5602@gmail.com>
2021-06-20 17:51:53 +0530

    Update profile feature is optimized
    New method editAccount_view() is added.
    editContact_view() and editProfile_view() is removed.
    URL is intacted

--- mmnotes/accounts/urls.py
+++ mmnotes/accounts/urls.py
@@ -2,14 +2,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import login_view, register_view, logout_view, profile_view, editprofile_view, editContact_view, editAccount_view
+from .views import login_view, register_view, logout_view, profile_view, editAccount_view
 
 urlpatterns = [
     path('profile/', profile_view, name='userProfile'),
     path('login/', login_view, name='userLogin'),
     path('register/', register_view, name='userRegister'),
     path('logout/', logout_view, name='userLogout'),
-    path('editprofile/', editprofile_view, name='editprofile'),
-    path('editcontact/', editContact_view, name='editcontact'),
     path('editaccount/', editAccount_view, name='editaccount'),
 ]
--- mmnotes/accounts/views.py
+++ mmnotes/accounts/views.py
@@ -46,9 +46,12 @@ def register_view(request):
         user = form.save(commit=False)
         password = form.cleaned_data.get('password')
         user.set_password(password)
+        mob = ''
         userSub = UserSubscription(username=user, subscription_details=default_pack, subject_details=default_access)
+        UserContactInfo = UserContact.objects.create(username=user.username, contact_number=mob)
         user.save()
         userSub.save()
+        UserContactInfo.save()
         new_user = authenticate(username=user.username, password=password)
         login(request, new_user)
         if next:
@@ -61,6 +64,7 @@ def register_view(request):
     }
     return render(request, "accounts/signup.html", context)
 
+'''
 @login_required
 def editprofile_view(request):
     form = EditUserProfileForm(request.POST or None)
@@ -121,7 +125,7 @@ def editContact_view(request):
                 'footer': 0,
             }
             return render(request, 'accounts/editcontact.html', context)
-
+'''
 def logout_view(request):
     logout(request)
     return redirect('/')
@@ -129,32 +133,96 @@ def logout_view(request):
 @login_required
 def profile_view(request):
     currentUser = request.user
-    isNameNone = False
-    isContactNone = False
+    isAlert = False
     subscription = UserSubscription.objects.get(username=currentUser.id)
     contact = UserContact.objects.filter(username=str(currentUser.username))
-    #print("---------------", contact)
+    #print("--------------->", contact)
     accessType = subscription.subscription_details
     subjectAccess = subscription.subject_details
     if currentUser.first_name == "" and currentUser.last_name == "":
         #print("----------------------- Incomplete profile -----------------------")
-        isNameNone = True 
-    if contact.exists():
+        isAlert = True 
+    if contact.exists() and contact:
         print("----------------------- contact found -----------------------")
-        print("---------------", contact)
+        #print("--------------->", contact)
+        currentContact = UserContact.objects.get(username= currentUser.username)
+        #print("----------------> User Contact Details :", currentContact.contact_number)
+        if currentContact.contact_number == '':
+            #print("----------------------- No contact found -----------------------")
+            isAlert = True
     else:
-        print("----------------------- No contact found -----------------------")
-        isContactNone = True
+        #print("----------------------- No contact found -----------------------")
+        isAlert = True
     context = {
         'layout': 0,
         'footer': 0,
         'accessType': str(accessType),
         'subject': str(subjectAccess),
-        'nameAlert': isNameNone,
-        'contactAlert': isContactNone,
+        'user_contact': currentContact.contact_number,
+        'alert': isAlert,
     }
     return render(request, "accounts/profile.html", context)
 
-
+@login_required
 def editAccount_view(request):
-    return render(request, 'accounts/editAccount.html', context=None)
+    if request.method == 'POST':
+        errorMsg = ''
+        #requestUser = request.user
+        form_data = request.POST
+        #print("------------------------- Form Data --------------------------")
+        if form_data['fname'] != '' or form_data['lname'] != '' and form_data['contactNum'] != '' :
+            #print("------------------------------------ Saving User Info ----------------------------")
+            user = User.objects.get(id=request.user.id)
+            fname = form_data['fname']
+            lname = form_data['lname']
+            user.first_name = str(fname)
+            user.last_name = str(lname)
+            user.save()
+            #print("--------------------------- First Name and Last Name is saved")
+            mob = form_data['contactNum']
+            contact = UserContact.objects.filter(username=str(request.user.username))
+            #print("---------------------------> ", contact)
+            if contact:
+                #print("----------------------------------- Updating Old Contact -----------------------------------")
+                currentContact = UserContact.objects.get(username= str(request.user.username))
+                currentContact.contact_number = mob
+                currentContact.save()
+                return redirect(profile_view)
+            else:
+                #print("----------------------------------- Creating New User Contact -----------------------------------")
+                contact = UserContact.objects.create(username=str(request.user.username), contact_number=mob)
+                contact.save()
+                return redirect(profile_view)
+        else:
+            print("------------------------ Error Occured! please try again! ------------------------")
+            errorMsg = "Something went wrong! please try again!"
+            context = {
+            'footer': 0,
+            'layout':0,
+            'error': errorMsg
+            }
+            return render(request, 'accounts/editAccount.html', context)
+    else:
+        currentUser = request.user
+        currentContact = ''
+        nameUpdate = False
+        contactUpdate = False
+        contact = UserContact.objects.filter(username=str(currentUser.username))
+        if contact.exists():
+            #print("------------------ Contact found ------------------")
+            currentContact = UserContact.objects.get(username=currentUser.username)
+            #print("-----------------> Contact :", currentContact.contact_number)
+        else:
+            #print("------------------ Contact not found ------------------")
+            contactUpdate = True
+        if currentUser.first_name == "" and currentUser.last_name == "":
+            #print("------------------ Name not found ------------------")
+            nameUpdate = True
+        context = {
+            'footer': 0,
+            'layout':0,
+            'nameUpdate' : nameUpdate,
+            'contactUpdate' : contactUpdate,
+            'contact': currentContact.contact_number,
+            }
+        return render(request, 'accounts/editAccount.html', context)
